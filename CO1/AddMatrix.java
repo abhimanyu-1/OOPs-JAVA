91871908e7ad445f9d7d8343bf6aa24dfe926795
abhimanyu <116670401+abhimanyu-1@users.noreply.github.com>
2023-03-18 11:18:37 +0530

    Add files via upload

--- CO1/AddMatrix.java
+++ CO1/AddMatrix.java
@@ -1,45 +1,52 @@
 import java.util.Scanner;
-
-public class AddMatrix{
+class AddMatrix{
+  public static void main(String args[]){
+  	int i,j;
+        int[][] a = new int[10][10];
+        int[][] b = new int[10][10];
+        int[][] c = new int[10][10];
+        
+	Scanner s = new Scanner(System.in);
+	int m,n;
+        System.out.println("Enter the size of the matrix ");
+        m = s.nextInt();
 	
-	public static void main(String[] args) {
-		
-		Scanner s= new Scanner(System.in);
-		
-		int[][] a= new int[3][3];
-		int[][] b= new int[3][3];
-		int[][] c= new int[3][3];
-		
-		System.out.println("Enter the size of first matrix :");
-		int s1=s.nextInt();
-		int s2=s.nextInt();
-		
-		System.out.println("Enter the matrix :");
-		for(int i=0;i<s1;i++) {
-			for(int j=0;j<s2;j++) {
-				a[i][j]=s.nextInt();
-			}
-		}
-		
-		System.out.println("Enter the next matrix :");
-		for(int i=0;i<s1;i++) {
-			for(int j=0;j<s2;j++) {
-				b[i][j]=s.nextInt();
-			}
-		}
-		for(int i=0;i<s1;i++) {
-			for(int j=0;j<s2;j++) {
-				
-				c[i][j]=a[i][j]+b[i][j];
-			}
-		}
-	System.out.println("Sum matrix:");
-		for(int i=0;i<s1;i++) {
-			System.out.println();
-			for(int j=0;j<s2;j++) {
-				System.out.println(" "+c[i][j]+" ");
-			}
-			System.out.println();
-		}
-}
+        n = s.nextInt();
+	
+	System.out.println("Enter the Value for Matrix A :");
+	for(i=0;i<m;i++)
+        {
+	 for(j=0;j<n;j++)
+         {
+ 	   a[i][j] = s.nextInt();
+         }
+       }
+	
+     
+       System.out.println("Enter the Value for Matrix B ");
+	for(i=0;i<m;i++)
+        {
+	 for(j=0;j<n;j++)
+          {
+ 	    b[i][j] = s.nextInt();
+         }
+       }
+	
+
+	for(i=0;i<m;i++)
+         {
+	 for(j=0;j<n;j++)
+          {
+ 	   c[i][j] = b[i][j]+a[i][j];
+         }
+       }
+ 
+     System.out.println("Result");
+     for(i=0;i<m;i++){
+	 for(j=0;j<n;j++){
+ 		System.out.print(" "+c[i][j]+" ");
+         }
+         System.out.println();
+       }	
+  }
 }
